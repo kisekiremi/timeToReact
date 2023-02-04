b13905ba6d2378666e31e63277306d4a34acccfe
kisekiremi <kisekiremi@outlook.com>
2023-02-04 12:42:27 +0800

    添加记录workingTIme

--- src/views/smalltalk.tsx
+++ src/views/smalltalk.tsx
@@ -3,8 +3,10 @@ import '../style/modules/smalltalk.scss'
 
 export default function smalltalk() {
   return (
-    <div className="smalltalk flex-c">
-      <span>smalltalk</span>
+    <div className="smalltalk flex-c h-full">
+      <a href="https://docs.qq.com/sheet/DRmFSSVFoR1R3bFFU?tab=BB08J2" target="_blank" rel="noopener noreferrer">
+        <b className="text-lg text-red-600 underline decoration-double">Jump to workingTime</b>
+      </a>
     </div>
   )
 }
