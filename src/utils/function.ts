e281032539fe91a31bb5e068dfb7bb6df3af74f8
songyuehu <songyuehu@jumpw.com>
2023-08-21 16:41:11 +0800

    update audio active canvas

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,3 +1,9 @@
-export function praseTime() {
-  // TODO
+// 随机数
+export function produceNum(min: number, max: number, nums: number) {
+  let arr = []
+  for (let i = 0; i < nums; i++) {
+    let num = Math.floor(Math.random() * (max - min) + min)
+    arr.indexOf(num) == -1 ? arr.push(num) : i-- // 不存在push入，存在则使i-1增加一次循环次数
+  }
+  return arr
 }
--- src/views/haru.tsx
+++ src/views/haru.tsx
@@ -1,10 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../style/modules/haru.scss'
+import { produceNum } from '../utils/function'
+// @ts-ignore
+import budff from '../assets/audio/BeMax - So Kawaiina (Zero Two).mp3'
+
+async function initHaru() {
+  console.log('start')
+
+  const listenAudioRef = document.getElementById('listenAudio') as HTMLMediaElement
+  listenAudioRef.volume = 0.15
+
+  const cvs = document.getElementById('newCanvas') as HTMLCanvasElement
+  const ctx = cvs.getContext('2d')
+
+  let isInit = false
+  // 创建音源分析器   创建音源对象
+  let audioAnalysis: AnalyserNode
+  // 创建存储音频数据数组，用于存储分析器传递的音频信息
+  let bufferLength: Uint8Array
+
+  // 开画
+  listenAudioRef.onplay = () => {
+    // 创建音源节点
+    const audioEl = new AudioContext()
+    // 链接音源
+    const audioSource = audioEl.createMediaElementSource(listenAudioRef)
+    // 将音源节点传递给音频分析器
+    audioAnalysis = audioEl.createAnalyser()
+    // 设置分析器设定，设定为2的幂次方，次方越多显示的柱状条越多，默认2048。地址：https://developer.mozilla.org/zh-CN/docs/Web/API/AnalyserNode/fftSize
+    audioAnalysis.fftSize = 32 // 也就是16条柱子
+    // 存储分析器导出的分析音频数组
+    bufferLength = new Uint8Array(audioAnalysis.frequencyBinCount)
+    // 连接分析器和节点
+    audioSource.connect(audioAnalysis)
+    // 分析器链接至音频最终目标源（通常可以指代为扬声器/音频输出源）
+    audioAnalysis.connect(audioEl.destination)
+    // console.log('bufferLength', bufferLength)
+    isInit = true
+    console.log('render')
+    render()
+  }
+
+  function render() {
+    requestAnimationFrame(render)
+
+    if (isInit && ctx) ctx.clearRect(0, 0, cvs.width, cvs.height)
+    else return
+
+    // 实时获取更新bufferLength的数据
+    audioAnalysis.getByteFrequencyData(bufferLength)
+
+    // 拿到单条条形柱的宽，bufferLength的长度是 = audioAnalysis.fftSize / 2
+    const barW = cvs.width / bufferLength.length
+
+    ctx.shadowColor = '#003333'
+    ctx.shadowBlur = 5
+    ctx.fillStyle = 'rgb(' + produceNum(1, 255, 1) + ',' + produceNum(1, 255, 1) + ',' + produceNum(1, 255, 1) + ')'
+    // ctx.strokeStyle = '#'
+    // 绘制bar条
+    for (let i = 0; i < bufferLength.length; i++) {
+      const barH =
+        (bufferLength[i] / 255) * cvs.height > cvs.height * 0.98
+          ? cvs.height * 0.98
+          : (bufferLength[i] / 255) * cvs.height
+      const trueBarW = barW - 10
+      const x = i * barW + barW / 2 - trueBarW / 2 // transform(translate(-50%,-50%))
+      const y = cvs.height - barH
+      ctx.fillRect(x, y, trueBarW, barH)
+    }
+  }
+}
 
 export default function haru() {
+  useEffect(() => {
+    initHaru()
+    return () => {
+      //
+    }
+  }, [initHaru])
+
   return (
-    <div className="haru flex-c">
-      <b>haru</b>
+    <div className="haru flex-c flex-wrap flex-col">
+      <>
+        <audio id="listenAudio" src={budff} crossOrigin="anonymous" controls preload="" loop></audio>
+        <br />
+        <canvas id="newCanvas"></canvas>
+      </>
     </div>
   )
 }
