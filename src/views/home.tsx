ae1a730e7f9a35473ab3bf7fa8e4c07cf7e65c05
kisekiremi <kisekiremi@outlook.com>
2025-04-14 10:49:30 +0800

    上传，接口则加上域名限制

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -6,16 +6,20 @@ import { readVisitTime, writeVisitTime } from '../api/visit'
 
 export default function () {
   const UseNavigate = useNavigate()
-  const [visitTime, setVisitTime] = useState(0)
 
-  useEffect(() => {
-    // TODO：现在就是有一个跨域问题在这里，需要处理
-    // const data: any = readVisitTime()
-    // if (data) {
-    //   console.log('xxx', data)
-    //   setVisitTime(data)
-    // }
-  }, [visitTime])
+  // const [visitTime, setVisitTime] = useState(0)
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const data: any = await readVisitTime()
+  //     if (data.code === 0) {
+  //       console.log('xxx', data)
+  //       setVisitTime(data.value)
+  //       await writeVisitTime()
+  //     }
+  //   }
+  //   fetchData()
+  // }, [])
 
   return (
     <div className="Home flex-c flex-col bg-red-400 w-full min-h-full">
@@ -25,9 +29,10 @@ export default function () {
         click to 穿梭
       </b>
 
+      {/* 
       <div className="absolute left-1/2  -translate-x-1/2 text-white" style={{ top: '120px' }}>
         visit_time: {visitTime}
-      </div>
+      </div> */}
     </div>
   )
 }
